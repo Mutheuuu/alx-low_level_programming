dac8865eaea05686f33617264ca974dea0b12e4d
Abigael Mutheu <abigailmutheu3@gmail.com>
2022-09-11 12:05:28 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,16 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-#include <stdio.h>
-
-
-
 /**
  *
- *  * main - assigns a random number to int n everytime
+ *  * main - entry point
  *
- *   * it executes, and prints it
+ *   * Description: prints  if a number is positive or negative.
  *
- *    * Return: Always 0 (Success)
+ *    * Return: 0
  *
  *     */
 
@@ -30,59 +28,27 @@ int main(void)
 
 					if (n > 0)
 
-								printf("%d is positive\n", n);
+							{
+
+										printf("%d is positive\n", n);
+
+											}
 
 						else if (n == 0)
 
-									printf("%d is zero\n", n);
+								{
+
+											printf("%d is zero\n", n);
+
+												}
 
 							else if (n < 0)
 
-										printf("%d is negative\n", n);
+									{
 
-								return (0);
+												printf("%d is negative\n", n);
 
-}#include <stdlib.h>
-
-#include <time.h>
-
-#include <stdio.h>
-
-
-
-/**
- i*
- *  * main-assigns a random number to int n everytime
- *
- *   * it executes, and prints it
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					if (n > 0)
-
-								printf("%d is positive\n", n);
-
-						else if (n == 0)
-
-									printf("%d is zero\n", n);
-
-							else if (n < 0)
-
-										printf("%d is negative\n", n);
+													}
 
 								return (0);
 
