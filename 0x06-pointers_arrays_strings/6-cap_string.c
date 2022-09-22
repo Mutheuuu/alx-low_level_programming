af470a1433bf366fdc6cdf3f666a9b51938b36aa
Abigael Mutheu <abigailmutheu3@gmail.com>
2022-09-22 01:44:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *cap_string - capitalizes all words in a string
- *@s: string to capitalize
- *Return: address of s
+ *cap_string-capitalizes all words in a string
+ *@s:string to capitalize
+ *Return:address of s
  */
 
 char *cap_string(char *s)
