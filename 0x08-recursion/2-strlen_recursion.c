9ebc5f24a4c4be3721d713a5afa9608a2babd8f0
Abigael Mutheu <abigailmutheu3@gmail.com>
2022-09-28 06:12:42 -0700

    4-pow_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-int sum = 0;
-if  (*s != '\0')
-sum++;
-sum += _strlen_recursion(s + 1);
-return (sum);
+if  (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
 }
