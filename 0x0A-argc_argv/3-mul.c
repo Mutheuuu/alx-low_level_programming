f30771e14ed7c5066cff35f221d434142417cf79
Abigael Mutheu <abigailmutheu3@gmail.com>
2022-11-07 11:41:49 -0800

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - multiplies two numbers
  * @argc: prints all arguments passed in a program
  * @argv: an array containing all arguments passed in a program
- * Return : 1 if not program does not receive 2 arguments
+ * Return: 1 if not program does not receive 2 arguments
  */
 
 int main(int argc, char *argv[])
