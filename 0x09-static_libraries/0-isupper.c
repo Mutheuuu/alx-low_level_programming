6db77c226a4ebdb828ccdc22ee5333cdc98eaea8
Abigael Mutheu <abigailmutheu3@gmail.com>
2022-11-12 11:54:38 -0800

    static library libmy.a created

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,8 +2,11 @@
 
 /**
  * *_isupper - checks if a  character is uppercase or not
- *@c: character to be tested
- *Return: 1 whether it is, 0 otherwise
+ *
+ * *@c: character to be tested
+ *
+ * *Return: 1 whether it is, 0 otherwise
+ *
  */
 
 int _isupper(int c)
