516461a4f08c9bfca7cfb8a365c3dfb1d532ecbe
Abigael Mutheu <abigailmutheu3@gmail.com>
2023-01-20 10:37:02 -0800

    libdynamic.so and headerfile added

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Copy b in s n times
+ * @s: variable copied into
+ * @b: variable copied into s
+ * @n: number of times b is copiedd
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+unsigned int i = 0;
+for (; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
